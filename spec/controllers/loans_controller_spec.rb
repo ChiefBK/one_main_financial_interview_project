08366e1b29c58231aec350bac8cf8f82e713d4d6
Ian Pierce <ian.pierce@hey.com>
2021-04-19 23:55:28 -0500

    test cases for #index and #show

--- spec/controllers/loans_controller_spec.rb
+++ spec/controllers/loans_controller_spec.rb
@@ -2,9 +2,14 @@ require 'rails_helper'
 
 RSpec.describe LoansController, type: :controller do
   describe '#index' do
+    let!(:loan_1) { Loan.create!(funded_amount: 100.0) }
+    let!(:loan_2) { Loan.create!(funded_amount: 101.0) }
     it 'responds with a 200' do
       get :index
+      body = JSON.parse(response.body)
+
       expect(response).to have_http_status(:ok)
+      expect(body.length).to eq(2)
     end
   end
 
@@ -13,7 +18,11 @@ RSpec.describe LoansController, type: :controller do
 
     it 'responds with a 200' do
       get :show, params: { id: loan.id }
+      body = JSON.parse(response.body)
+
       expect(response).to have_http_status(:ok)
+      expect(body.class).to eq(Hash) # should only have one record in response
+      expect(body['id']).to eq(loan.id)
     end
 
     context 'if the loan is not found' do
