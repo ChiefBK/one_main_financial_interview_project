b5eb980ccb1d67c99c9e1718057dcd924e819f10
Ian Pierce <ian.pierce@hey.com>
2021-04-19 23:55:42 -0500

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :loans, defaults: {format: :json}
+  resources :loans, defaults: {format: :json}, only: [:show, :index] do
+    resources :payments, only: [:show, :index, :create]
+  end
 end
