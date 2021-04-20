f3c0ed13c62fdce62b717fc93d8ff4e36bf3e8ec
Ian Pierce <ian.pierce@hey.com>
2021-04-19 23:53:51 -0500

    catches exceptions and returns json response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    errors = [exception.message]
+    render json: errors, status: :not_found
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |exception|
+    errors = [exception.message]
+    render json: errors, status: :unprocessable_entity
+  end
 end
