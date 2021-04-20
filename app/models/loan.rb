73025379643d38a06e72018eeaa261bad5777c92
Ian Pierce <ian.pierce@hey.com>
2021-04-19 23:54:54 -0500

    added relationships; new method

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,2 +1,8 @@
 class Loan < ActiveRecord::Base
+  has_many :payments
+
+  def remaining_loan_amount
+    payment_amounts = payments.map(&:amount)
+    funded_amount - payment_amounts.sum
+  end
 end
